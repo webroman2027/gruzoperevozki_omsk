7579510e125caadc259cde08611f69ae78981453
webroman2027 <82033168+webroman2027@users.noreply.github.com>
2021-04-06 17:39:59 +0600

    Грузоперевозки Омск
    Грузоперевозки Омск, доставка/перевозка сыпучих и других грузов

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Грузоперевозки Омск</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="icon/iconfonts.css">
+    <link rel="stylesheet" href="css/media.css">
+    <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
+    <meta name="description" content="Грузоперевозки Омск, доставка/перевозка грузов">
+    <meta name="keywords" content="грузоперевозки, грузоперевозки омск недорого, доставка, перевозка грузов, доставка сыпучех грузов, песок, уголь, мусор">   
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+                <h1 class="logo">Грузоперевозки по Омску<br>и Омской области</h1>
+            <nav class="nav__body">
+                <ul class="nav">
+                    <li class="nav__item flex">
+                        <i class="icon-location"></i>
+                        <address><p>г. Омск<br>ул 25 Северная</p></address>
+                    </li>
+                    <li class="nav__item">
+                        <i class="icon-phone"></i>  
+                        <a href="tel: +79081002154">+7(908)-100-21-54 </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+        <h1 class="main__title">Услуги</h1>
+         <hr class="hr__main">
+                <p class="title">Автомобиль ЗИЛ, вместимость: 4-9 м3, 5,5 тонн длина 3 м ширина 210 высота борта 80</p>
+        <section class="main__section">
+             <div class="tarif__titels__main">
+                <p>Наименование услуги</p>
+                <p>Цена</p>
+             </div>
+             <hr class="hr__main full">
+             <div class="tarifs__block">
+                   <h3>Доставка/Перевозка</h3> 
+             <div class="tarifs">
+                <div class="tarif__title">
+                    Песок
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Щебень
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Керамзит
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Вывоз снега
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Земля
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Перегной
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Шлак
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Супесь
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Acфальт
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Глина
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Уголь
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Опилки
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+            <div class="tarifs">
+                <div class="tarif__title">
+                    Вывоз мусора
+                </div>
+                <div class="tarif__title">
+                    от 700Р
+                </div>
+            </div>
+        </div>
+            <div class="text">Не нашли что искали? Звоните по номеру:</div>
+            <ul>
+                <li class="submain">
+                    <i class="icon-phone"></i>  
+                    <a class="number__main"  href="tel: +79081002154">+7(908)-100-21-54 </a>
+                </li>
+            </ul>
+         </section>
+    </main>
+
+    <footer>
+        <div class="container footer">
+        <h1 class="logo">Грузоперевозки по Омску<br>и Омской области</h1>
+            <nav class="nav__body">
+                <ul class="nav">
+                    <li class="nav__item flex">
+                        <i class="icon-location"></i>
+                        <address><p>г. Омск<br>ул 25 Северная</p></address>
+                    </li>
+                    <li class="nav__item">
+                        <i class="icon-phone"></i>  
+                        <a class="number"  href="tel: +79081002154">+7(908)-100-21-54</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <hr class="footer__hr">
+        <div class="footer__container">
+        <h1 class="footer__title">ГрузоперевозкиОмск55рус © 2021</h1>
+        </div>
+        
+    </footer>
+
+</body>
+</html>
